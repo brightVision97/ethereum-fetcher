a1ec4a861ec68dc3bc2058548f27244c5df7b571
Ivan Rachev <ivan.rachev@modis.com>
2023-05-04 13:35:42 +0300

    forgot to include resources and docker files new versions

---
+++ src/main/resources/insertUsers.sql
@@ -0,0 +1,5 @@
+insert into "public"._user(id, username, password) values (1, 'admin', '$2a$10$8.h1S099TCUh8JfXG.MiduaCl0bI8GSERz7hdGNAYvetANHcUfiw2');
+insert into "public"._user(id, username, password) values (2, 'alice', '$2a$10$98j9v6c.jwlfdprBqdTPieEfSXNKvxvt6/JzpLgdmmN8Eon.nw276');
+insert into "public"._user(id, username, password) values (3, 'bob', '$2a$10$Tc97AB4ARRYndeI13YfkFu1V2zkKxNu8JYBPu4KdlizPYEGGGubmy');
+insert into "public"._user(id, username, password) values (4, 'carol', '$2a$10$kF.HQ58zKD9890vQHTVdPuxTrJywQ4jLYd1e8pi3lXNS1yDoU2W6S');
+insert into "public"._user(id, username, password) values (5, 'dave', '$2a$10$utLpi/NbdCSgGBhvENLSJ.AmbNR14GCaTQJg9vq4iS3RRnupdF/qO');
