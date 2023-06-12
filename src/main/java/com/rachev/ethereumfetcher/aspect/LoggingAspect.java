bf9fa91598f83990750bccf02f8325d17c8555d9
Ivan Rachev <ivan.rachev@modis.com>
2023-06-12 14:00:02 +0300

    improved aspect logging

--- src/main/java/com/rachev/ethereumfetcher/aspect/LoggingAspect.java
+++ src/main/java/com/rachev/ethereumfetcher/aspect/LoggingAspect.java
@@ -1,67 +1,48 @@
 package com.rachev.ethereumfetcher.aspect;
 
+import com.rachev.ethereumfetcher.model.auth.AuthResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Component
 @Aspect
 @Slf4j
 public final class LoggingAspect {
 
-    private static final String API_POINT_CUT = "execution(* com.rachev.ethereumfetcher.controller.*.*(..))";
-
-    private static final String EXCEPTION_POINT_CUT = "execution(* com.rachev.ethereumfetcher.controller.*.*.*(..))";
-
-    private enum DataType {
-        REQUEST,
-        RESPONSE,
-    }
-
-    @Pointcut(API_POINT_CUT)
-    public void logController() {
-    }
-
-    @Around("logController()")
-    public Object logRequest(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Before("execution(* com.rachev.ethereumfetcher.controller.*.*(..))")
+    private String createJoinPointForLogs(JoinPoint joinPoint) {
         log.info(joinPoint.getSignature().getName());
-        log.info(createJoinPointForLogs(joinPoint, DataType.REQUEST));
-        return joinPoint.proceed(joinPoint.getArgs());
-    }
-
-    private String createJoinPointForLogs(JoinPoint joinPoint, DataType dataType) {
         Object[] args = joinPoint.getArgs();
         if (ArrayUtils.isEmpty(args)) {
             return joinPoint.getSignature().getName().concat(" method has no parameters");
         }
-        StringBuilder requestValue = new StringBuilder();
-        if (dataType.equals(DataType.REQUEST)) {
-            requestValue.append("\r\n========== The request values ==========");
-        } else {
-            requestValue.append("\r\n========== The response values ==========");
-        }
         return Arrays.stream(args)
-                .filter(Objects::nonNull)
-                .map(arg -> requestValue.append("\r\n")
-                        .append(arg)
-                        .append("\r\n=======================================")
-                        .toString())
-                .reduce("\r\n", (a, b) -> requestValue.append(a).append(b).toString());
+                .map(String::valueOf)
+                .collect(Collectors.joining(
+                        "\r\n",
+                        "\r\n========== The request values ==========\n",
+                        "\r\n=======================================")
+                );
     }
 
-    @AfterReturning("logController()")
-    public void logsResponse(JoinPoint joinPoint) {
-        log.info(joinPoint.getSignature().getName());
-        log.info(createJoinPointForLogs(joinPoint, DataType.RESPONSE));
+    @AfterReturning(
+            pointcut = "execution(* com.rachev.ethereumfetcher.service.AuthenticationServiceImpl.*(..))",
+            returning = "authResponse"
+    )
+    public void logsResponse(AuthResponse authResponse) {
+        log.info(authResponse.toString());
     }
 
-    @AfterThrowing(value = EXCEPTION_POINT_CUT, throwing = "exception")
+    @AfterThrowing(pointcut = "execution(* com.rachev.ethereumfetcher.controller.*.*.*(..))", throwing = "exception")
     public void logsErrors(JoinPoint joinPoint, Throwable exception) {
         log.info("========================== We have Error here ==========================");
         log.info(joinPoint.getSignature().getName());
